46b6b2c1a36bc463a1e6a12f0c6453413d805088
BrayanSLL <60930402+BrayanSLL@users.noreply.github.com>
2024-01-16 16:40:41 +0100

    Update Mouvement.java

--- projet-web/src/main/java/projet/jsf/data/Mouvement.java
+++ projet-web/src/main/java/projet/jsf/data/Mouvement.java
@@ -1,5 +1,89 @@
 package projet.jsf.data;
 
-public class Mouvement {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import projet.ejb.data.Utilisateur;
+
+@SuppressWarnings("serial")
+public class Mouvement implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    @NotNull(message = "L'utilisateur doit être renseignées")
+    private Utilisateur utilisateur;
+    @NotNull(message = "Le produit doit être renseignées")
+    private Produit produit;
+    private String type;
+    @NotNull(message = "Le montant doit être renseignées")
+    private double montant;
+    @NotNull(message = "La date et heure doivent être renseignées")
+    private Date dateHeure;
+
+    // Constructeurs, getters et setters
+
+    public Mouvement() {
+    }
+
+    public Mouvement(Utilisateur utilisateur, Produit produit, String type, double montant, Date dateHeure) {
+        this.utilisateur = utilisateur;
+        this.produit = produit;
+        this.type = type;
+        this.montant = montant;
+        this.dateHeure = dateHeure;
+    }
+
+    // Getters & setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
+
+    public Date getDateHeure() {
+        return dateHeure;
+    }
+
+    public void setDateHeure(Date dateHeure) {
+        this.dateHeure = dateHeure;
+    }
 }
